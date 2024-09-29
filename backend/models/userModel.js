9fc0aa9e3c9b7095014afec43c4733010000e0be
ca <achinga.chris@gmail.com>
2024-09-29 12:18:15 +0300

    Refactor userModel.js to add phone field

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -12,6 +12,11 @@ const userSchema = mongoose.Schema(
 			required: true,
 			unique: true,
 		},
+		phone: {
+			type: String,
+			required: true,
+			unique: true,
+		},
 		password: {
 			type: String,
 			required: true,
