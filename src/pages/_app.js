4b4a9706ca400c6f927793a5526d61e22d15166d
ca <achinga.chris@gmail.com>
2025-01-28 11:48:02 +0300

    feat: Add Preline integration and create Form component with styled inputs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import PrelineScript from "@/utils/PrelineScript";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <PrelineScript />
+    </>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,114 +1,135 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
+  const year = new Date().getFullYear()
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-white">
+      <main
+        id='content'
+        className="relative max-w-3xl px-4 sm:px-6 lg:px-8 flex flex-col justify-center items-center mx-auto size-full"
+      >
+        <div className='text-center py-8 px-4 sm:px-6 lg:px-8'>
+          <h1 className='text-2xl text-[#001F94] sm:text-4xl'>
+            Get notified when we launch
+          </h1>
+          <h2 className='mt-1 sm:mt-3 text-4xl font-bold text-[#001F94] sm:text-6xl'>
+            <span className='bg-clip-text bg-gradient-to-tr from-[#00988A] to-[#001F94] text-transparent'>
+              Mashabiki Sports House
+            </span>
+          </h2>
+          <form>
+            <div className='mt-8 space-y-4'>
+              <div>
+                <label
+                  htmlFor='hs-cover-with-gradient-form-name-1'
+                  className='sr-only'
+                >
+                  Full name
+                </label>
+                <div className='relative'>
+                  <input
+                    type='text'
+                    id='hs-cover-with-gradient-form-name-1'
+                    className='py-3 ps-11 pe-4 block w-full bg-gray-100 border-gray-200 text-gray-900 placeholder:text-gray-400 rounded-lg text-sm focus:border-[#00988A] focus:ring-[#00988A] sm:p-4 sm:ps-11'
+                    placeholder='Full name'
+                  />
+                  <div className='absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4'>
+                    <svg
+                      className='shrink-0 size-4 text-gray-400'
+                      xmlns='http://www.w3.org/2000/svg'
+                      width={24}
+                      height={24}
+                      viewBox='0 0 24 24'
+                      fill='none'
+                      stroke='currentColor'
+                      strokeWidth={2}
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                    >
+                      <path d='M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2' />
+                      <circle cx={12} cy={7} r={4} />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor='hs-cover-with-gradient-form-email-1'
+                  className='sr-only'
+                >
+                  Email address
+                </label>
+                <div className='relative'>
+                  <input
+                    type='email'
+                    id='hs-cover-with-gradient-form-email-1'
+                    className='py-3 ps-11 pe-4 block w-full bg-gray-100 border-gray-200 text-gray-900 placeholder:text-gray-400 rounded-lg text-sm focus:border-[#00988A] focus:ring-[#00988A] sm:p-4 sm:ps-11'
+                    placeholder='Email address'
+                  />
+                  <div className='absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4'>
+                    <svg
+                      className='shrink-0 size-4 text-gray-400'
+                      xmlns='http://www.w3.org/2000/svg'
+                      width={24}
+                      height={24}
+                      viewBox='0 0 24 24'
+                      fill='none'
+                      stroke='currentColor'
+                      strokeWidth={2}
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                    >
+                      <rect
+                        width={20}
+                        height={16}
+                        x={2}
+                        y={4}
+                        rx={2}
+                      />
+                      <path d='m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7' />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div className='grid'>
+                <button
+                  type='submit'
+                  className='sm:p-4 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-[#00988A] text-white hover:bg-[#007F6E] focus:outline-none focus:bg-[#007F6E] disabled:opacity-50 disabled:pointer-events-none'
+                >
+                  Join the waitlist
+                  <svg
+                    className='shrink-0 size-4'
+                    xmlns='http://www.w3.org/2000/svg'
+                    width={24}
+                    height={24}
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth={2}
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                  >
+                    <path d='m9 18 6-6-6-6' />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      {/* ========== END MAIN CONTENT ========== */}
+      {/* ========== FOOTER ========== */}
+      <footer className='absolute bottom-0 inset-x-0 text-center py-5'>
+        <div className='max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8'>
+          <p className='text-sm text-gray-500'>
+            © {year} Mashabiki Sports House. Crafted by{' '}
+            <a
+              className='text-[#00988A] font-medium hover:text-[#007F6E] focus:outline-none focus:text-[#007F6E]'
+              href='https://www.chrisdevcode.co.ke/'
+            >
+              ChrisDevCode
+            </a>
+          </p>
+        </div>
       </footer>
     </div>
-  );
+  )
 }
